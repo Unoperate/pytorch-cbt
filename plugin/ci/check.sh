58f9f672cbd56e1dee356d588339b13c95ad10bb
Marek Dopiera <marek@dopiera.pl>
2021-08-31 19:45:23 +0200

    Add an end-to-end CI test.
    This test builds the whole solution, start the CBT emulator, creates a
    dummy table and executes a dumy example against that table.

---
+++ plugin/ci/check.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e -u -o pipefail
+
+THIS_SCRIPT_DIR=$( cd "$( dirname "$0" )" ; pwd )
+PROJECT_DIR=$( cd "$THIS_SCRIPT_DIR/.." ; pwd )
+
+EMULATOR_PATH=/usr/local/google-cloud-sdk/platform/bigtable-emulator/cbtemulator
+EMULATOR_PID=
+EMULATOR_LOG=
+
+onExit() {
+  [ -z "$EMULATOR_PID" ] || kill "$EMULATOR_PID"
+  [ -z "$EMULATOR_LOG" ] || rm -f "$EMULATOR_LOG"
+}
+
+log() {
+  echo "$@" > /dev/stderr
+}
+
+fail() {
+  log "$@"
+  exit 1
+}
+
+trap onExit exit
+
+cd "$PROJECT_DIR"
+python3 setup.py install
+
+EMULATOR_LOG=$(mktemp)
+"$EMULATOR_PATH" -host 127.0.0.1 -port 0 >"$EMULATOR_LOG" &
+EMULATOR_PID=$!
+
+# Wait until the CBT emulator spits out a line with:
+# "Cloud Bigtable emulator running on 127.0.0.1:12345"
+BIGTABLE_EMULATOR_HOST=$(
+  timeout 30 sh -c "tail -f \"$EMULATOR_LOG\" || true" \
+  | grep -im 1 "running on" \
+  | fmt -w 1 \
+  | grep '^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+:[0-9]\+$'
+  )
+
+[ -n "$BIGTABLE_EMULATOR_HOST" ] || fail "Failed to find the emulator port number"
+export BIGTABLE_EMULATOR_HOST
+
+log "Cloud Bigtable emulator is running on port $BIGTABLE_EMULATOR_HOST"
+
+# Set default CBT project and instance
+echo project = fake_project > ~/.cbtrc
+echo instance = fake_instance >> ~/.cbtrc
+# Create a dumy table.
+cbt createtable fake_table
+# Read something from it.
+LD_LIBRARY_PATH=/usr/local/lib/python3.8/dist-packages/torch/lib \
+  python3 \
+  pytorch_bigtable/example.py
+
+log "Successfully ran CBT pyTorch example."
--- plugin/pytorch_bigtable/example.py
+++ plugin/pytorch_bigtable/example.py
@@ -3,3 +3,10 @@ import pbt_C as code
 def get_data_py(project_id, instance_id, table_id):
     print("example function in python calling c++ code")
     code.get_data(project_id, instance_id, table_id)
+
+def main():
+    get_data_py('fake_project', 'fake_instance', 'fake_table')
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
