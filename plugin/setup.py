56df2deaf1a30794268461549c791e0586bd6140
Kajetan Boroszko <kajetan.boroszko@gmail.com>
2021-09-02 10:00:23 +0200

    use c++17 standard so we can use std::optional

--- plugin/setup.py
+++ plugin/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, Extension, find_packages
 from torch.utils import cpp_extension
 import os
 
-extra_compile_args = os.popen('pkg-config bigtable_client --cflags').read().split()
+extra_compile_args = os.popen('pkg-config bigtable_client --cflags').read().split() + ['-std=c++17']
 extra_link_args = os.popen('pkg-config bigtable_client --libs').read().split()
 
 setup(name='pytorch_bigtable',
       ext_modules=[cpp_extension.CppExtension(
             'pbt_C', 
-            ['csrc/dataset.cpp'], 
+            ['csrc/bigtable_dataset.cc'], 
             extra_compile_args=extra_compile_args,
             extra_link_args=extra_link_args,
             )],
