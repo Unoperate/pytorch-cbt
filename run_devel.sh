e96249106895cfb898aee93cf2b821cb18717a23
Kajetan Boroszko <kajetan@unoperate.com>
2021-10-01 07:04:06 +0200

    restored old dockerfile

--- run_devel.sh
+++ run_devel.sh
@@ -19,5 +19,5 @@ set -e -u -o pipefail
 ROOT_DIR=$( cd "$( dirname "$0" )" ; pwd )
 cd "$ROOT_DIR"
 
-docker build -t pythorch_cbt_centos .
-docker run --rm -it --volume "$ROOT_DIR/plugin:/opt/pytorch" pythorch_cbt_centos
+docker build -t pythorch_cbt_devel .
+docker run --rm -it --volume "$ROOT_DIR/plugin:/opt/pytorch" pythorch_cbt_devel
