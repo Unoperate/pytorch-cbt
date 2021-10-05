6835928bd793a32a20f6ddb705918f8916a2526e
Kajetan Boroszko <kajetan@unoperate.com>
2021-10-05 16:31:39 +0200

    fix build_wheels.sh

--- plugin/build_wheels.sh
+++ plugin/build_wheels.sh
@@ -20,13 +20,13 @@ ROOT_DIR=$( cd "$( dirname "$0" )" ; pwd )
 cd "$ROOT_DIR"
 OUTPUT_DIR=$ROOT_DIR/dist
 
-for python_path in $(find /opt/python/ | grep "cp3[6|7|8|9]")
+for python_path in $(find /opt/python -name "cp3[6789]*")
 do
   "$python_path/bin/pip" install torch==1.9.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
-  "$python_path/bin/python" setup.py bdist_wheel -d $OUTPUT_DIR
+  "$python_path/bin/python" setup.py bdist_wheel -d "$OUTPUT_DIR"
 done
 
-for wheel_file in $OUTPUT_DIR/*
+for wheel_file in "$OUTPUT_DIR/*"
 do
   mv "$wheel_file" "${wheel_file/linux/manylinux2014}"
 done
